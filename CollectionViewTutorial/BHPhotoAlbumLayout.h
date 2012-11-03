c5106400127372775ec36b108b2c41d7c7e65a86
Bryan Hansen <bryguy1300@gmail.com>
2012-11-03 13:44:17 -0700

    Adding collectionView layout stub.

---
+++ CollectionViewTutorial/BHPhotoAlbumLayout.h
@@ -0,0 +1,13 @@
+//
+//  BHPhotoAlbumLayout.h
+//  CollectionViewTutorial
+//
+//  Created by Bryan Hansen on 11/3/12.
+//  Copyright (c) 2012 Bryan Hansen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BHPhotoAlbumLayout : UICollectionViewLayout
+
+@end
---
+++ CollectionViewTutorial/BHPhotoAlbumLayout.m
@@ -0,0 +1,13 @@
+//
+//  BHPhotoAlbumLayout.m
+//  CollectionViewTutorial
+//
+//  Created by Bryan Hansen on 11/3/12.
+//  Copyright (c) 2012 Bryan Hansen. All rights reserved.
+//
+
+#import "BHPhotoAlbumLayout.h"
+
+@implementation BHPhotoAlbumLayout
+
+@end
