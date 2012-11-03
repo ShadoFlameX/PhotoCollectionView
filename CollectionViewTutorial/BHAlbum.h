292f76f0251ee779c1a2d224b58e8c58f33a5def
Bryan Hansen <bryguy1300@gmail.com>
2012-11-03 14:13:03 -0700

    Adding album model with add/remove photo.

---
+++ CollectionViewTutorial/BHAlbum.h
@@ -0,0 +1,20 @@
+//
+//  BHAlbum.h
+//  CollectionViewTutorial
+//
+//  Created by Bryan Hansen on 11/3/12.
+//  Copyright (c) 2012 Bryan Hansen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class BHPhoto;
+
+@interface BHAlbum : NSObject
+
+@property (nonatomic, strong, readonly) NSArray *photos;
+
+- (void)addPhoto:(BHPhoto *)photo;
+- (BOOL)removePhoto:(BHPhoto *)photo;
+
+@end
---
+++ CollectionViewTutorial/BHAlbum.m
@@ -0,0 +1,47 @@
+//
+//  BHAlbum.m
+//  CollectionViewTutorial
+//
+//  Created by Bryan Hansen on 11/3/12.
+//  Copyright (c) 2012 Bryan Hansen. All rights reserved.
+//
+
+#import "BHAlbum.h"
+#import "BHPhoto.h"
+
+@interface BHAlbum ()
+
+@property (nonatomic, strong) NSMutableArray *mutablePhotos;
+
+@end
+
+@implementation BHAlbum
+
+#pragma mark - Properties
+
+- (NSArray *)photos
+{
+    return [self.mutablePhotos copy];
+}
+
+#pragma mark - Photos
+
+- (void)addPhoto:(BHPhoto *)photo
+{
+    [self.mutablePhotos addObject:photo];
+}
+
+- (BOOL)removePhoto:(BHPhoto *)photo
+{
+    if ([self.mutablePhotos indexOfObject:photo] == NSNotFound) {
+        return NO;
+    }
+    
+    [self.mutablePhotos removeObject:photo];
+    
+    return YES;
+}
+
+
+
+@end
